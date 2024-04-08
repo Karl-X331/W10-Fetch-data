a0c33c2c4d0f3249303ec137431ddfbf1437019f
= <=>
2024-04-08 14:39:26 +1000

    Initial Commit

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,5 +1,5 @@
 export async function load(){
-    const response = await fetch("https://jsonplaceholder.typicode.com/user")
+    const response = await fetch("https://jsonplaceholder.typicode.com/users")
 
     if(!response.ok){
         return {success : false}
