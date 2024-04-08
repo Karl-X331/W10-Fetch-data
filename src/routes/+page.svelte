13398179161983971184ca32e1a1104cff3adfaa
= <=>
2024-04-08 14:32:11 +1000

    Initial Commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,11 +2,11 @@
     export let data
 </script>
 {#if data.success}
-<ul>
-    {#each data.myData as item}
-    <li>{item.name}</li>
-    {/each}
-</ul>
+    <ul>
+        {#each data.myData as item}
+          <li>{item.name}</li>
+         {/each}
+    </ul>
 {:else}
     <p>There was a problem getting the data...</p>
-{/else}
+{/if}
